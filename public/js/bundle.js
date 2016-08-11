4c85243dd9d37de3e950c8f8cffabaabb7457649
Andrew Jarrett <aj@Andrews-MacBook-Air.local>
2016-08-10 21:56:27 -0600

    added basic #routes to /about-us; todo: smooth-scroll

--- public/js/bundle.js
+++ public/js/bundle.js
@@ -226,4 +226,17 @@ $(function() {
 	// 	$('#collapseExample').attr('style', 'display: block');
 	// });
 
+  // $('.smooth-scroll').click(function() {
+  //   if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+  //     var target = $(this.hash);
+  //     target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+  //     if (target.length) {
+  //       $('html,body').animate({
+  //         scrollTop: target.offset().top 
+  //       }, 1000); // The number here represents the speed of the scroll in milliseconds
+  //       return false;
+  //     }
+  //   }
+  // });
+
 } )
